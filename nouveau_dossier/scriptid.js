31efde140bf92a6bfa465ac2d996f8ba0c43ee73
IssiakoService <154634142+IssiakoService@users.noreply.github.com>
2024-03-22 13:53:24 +0000

    Ajout d'un nouveau script au fichier JavaScript via JavaScript (scriptid.js)

--- nouveau_dossier/scriptid.js
+++ nouveau_dossier/scriptid.js
@@ -1,3 +1,4 @@
 1711114100075_wvbcl2w6bonjour<script> // Ajouter une nouvelle valeur à la clé 
 objetJS.IDU.push("nouvelle_valeur"); <script><script> // Ajouter une nouvelle valeur à la clé 
-objetJS.IDU.push <script>
+objetJS.IDU.push <script><script> // Ajouter une nouvelle valeur à la clé 
+objetJS.IDU.push(1711115605749_9easgso7); <script>
